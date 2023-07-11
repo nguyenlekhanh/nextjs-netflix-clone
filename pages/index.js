f2e1d66a7c57877d48c43f7ffe0014ee9275a398
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-11 15:22:37 -0500

    add banner

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
+import Banner from "@/components/banner/banner";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -10,6 +12,12 @@ export default function Home() {
       </Head>
 
       <h1>Netflix</h1>
+
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
     </div>
   );
 }
