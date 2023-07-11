312319c844c9565f2d31ca02de0df67aa8f002d2
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-11 15:37:57 -0500

    styling navbar

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,26 +1,34 @@
+import styles from "./navbar.module.css";
+
 const NavBar = (props) => {
-    const { username } = props;
-    return (
-      <div>
-        NavBar
-        <p>{username}</p>
-        <ul>
-          <li>Home</li>
-          <li>My List</li>
+  const { username } = props;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink}>
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
         </ul>
-        <nav>
+        <nav className={styles.navContainer}>
           <div>
-            <button>
-              <p>{username}</p>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
             </button>
-  
-            <div>
-              <a>Sign out</a>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
             </div>
           </div>
         </nav>
       </div>
-    );
-  };
-  
-  export default NavBar;
+    </div>
+  );
+};
+
+export default NavBar;
