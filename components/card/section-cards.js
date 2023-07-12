4aacef07e565412855242b98a53c2d4070a00fff
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-12 16:08:52 -0500

    apply youtube api

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,18 +1,24 @@
+import Link from "next/link";
 import Card from "./card";
 import styles from "./section-cards.module.css";
 
 const SectionCards = (props) => {
-  const { title } = props;
+  const { title, videos = [], size } = props;
+  console.log({ videos });
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
-        <Card imgUrl="/static/clifford.webp" size="large" />
-        <Card imgUrl="/static/clifford.webp" size="large" />
-        <Card imgUrl="/static/clifford.webp" size="large" />
-        <Card imgUrl="/static/clifford.webp" size="large" />
-        <Card imgUrl="/static/clifford.webp" size="large" />
-        <Card imgUrl="/static/clifford.webp" size="large" />
+        {videos.map((video, idx) => {
+          console.log({ video });
+          return (
+            <Link key={idx} href={`/video/${video.id}`}>
+              <span>
+                <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
+              </span>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
